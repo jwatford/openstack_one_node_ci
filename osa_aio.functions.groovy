2f386b8c42866459e3717a5d5629297426712600
Castulo J. Martinez <castulo.martinez@intel.com>
2016-12-06 14:21:45 -0800

    Copying the output directories only if exist

--- osa_aio.functions.groovy
+++ osa_aio.functions.groovy
@@ -220,9 +220,18 @@ def parse_persistent_resources_tests() {
 def aggregate_results(host_ip) {
 
     //Pull persistent, during, api, smoke results from onmetal to ES vm
+    host_ip = "${host_ip}".trim()
     sh """
-    scp -o StrictHostKeyChecking=no -r ubuntu@${host_ip}:\$HOME/output/ \$HOME
-    scp -o StrictHostKeyChecking=no -r ubuntu@${host_ip}:\$HOME/subunit/ \$HOME
+    {
+        scp -o StrictHostKeyChecking=no -r ubuntu@${host_ip}:\$HOME/output/ \$HOME
+    } || {
+        echo 'No output directory found.'
+    }
+    {
+        scp -o StrictHostKeyChecking=no -r ubuntu@${host_ip}:\$HOME/subunit/ \$HOME
+    } || {
+        echo 'No subunit directory found.'
+    }
     """
 
 }
